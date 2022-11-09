22410d34d5308f242d76c4b6c1b654bc685637b2
Francis Augusto Medeiros <register@francisaugusto.com>
2022-11-09 15:41:17 +0100

    Attempt to print a banner

--- src/pam_radius_auth.c
+++ src/pam_radius_auth.c
@@ -1356,7 +1356,7 @@ PAM_EXTERN int pam_sm_authenticate(pam_handle_t *pamh, UNUSED int flags, int arg
     
     if (config.banner){
         DPRINT(LOG_DEBUG, "A banner message is configured. Printing it.");
-        printf("%s",config.banner);
+        printf("Oh, no banner? %s",config.banner);
         
     }
 
